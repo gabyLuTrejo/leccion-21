93c73fc61ab38c513d8c8f04b6df30e1e11da541
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-17 10:54:05 -0500

    Imagenes con Modal

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+var img = document.getElementsByClassName("imagen");
+var lengthImg = img.length;
+
+for (var i = 0; i<lengthImg;i++) {
+	img[i].addEventListener('click', openModal)
+}
+
+function openModal(){
+	document.getElementById("imgModal").style.display = "block";
+	document.getElementById("imagenZoom").src= this.src
+}
+
+var span = document.getElementsByClassName("cerrar")[0];
+span.onclick = function() {
+    document.getElementById("imgModal").style.display = "none";
+}
